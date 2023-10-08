8062da7eeb444ce951e6e380895958d4cec95123
Souksanane <lithavong.souksanane@etu.univ-nantes.fr>
2023-10-08 22:20:53 +0200

    Mise à jour du CV

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experiences from './components/experience/Experience'
+import Education from './components/education/Education'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experiences/>
+      <Education/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './experience.css'
+
+const Experience = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Experience
